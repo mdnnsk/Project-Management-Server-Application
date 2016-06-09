dd61f1ce1922b50c038824fe279c6d6814581e98
HenryHall <HenryHall@Henrys-MacBook-Pro.local>
2016-06-09 11:59:19 -0500

    Current commit

--- scripts/script.js
+++ scripts/script.js
@@ -3,20 +3,24 @@ $(document).ready(function(){
 
   $('#createProject').click(function(){
     // var scope =
-    if (openProject = true) {
+    if (openProject == true) {
       alert("Please close your current project first.");
       return;
     }
 
-    // var projectName =
+    var projectName = "Test Project";
 
     var frontEnd = Math.floor(Math.random()*(60 - 10 + 1) + 10);
     var clientSide = Math.floor(Math.random()*(60 - 10 + 1) + 10);
     var serverSide = Math.floor(Math.random()*(60 - 10 + 1) + 10);
     // var requiredPoints = [frontEnd, clientSide, serverSide];
-    $('#projectIn').append("<h2>" + projectName + "</h2><p>Front End: " + frontEnd + "</p><p>Client Side: " + clientSide + "</p><p>Server Side: " + serverSide + "</p>")
+    $('#projectIn').append("<div><h2>" + projectName + "</h2><p>Front End: " + frontEnd + "</p><p>Client Side: " + clientSide + "</p><p>Server Side: " + serverSide + "</p><button id='deleteProject'>Delete Project</button></div>")
     openProject = true;
+    $('#deleteProject').click(function(){
+      $(this).parent().remove();
+      openProject = false;
 
+    });
   });
 
 });
