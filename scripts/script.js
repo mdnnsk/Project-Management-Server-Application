4d46d694763e2c7d98280bf9ff26a1bbe54cdb01
HenryHall <HenryHall@Henrys-MacBook-Pro.local>
2016-06-09 12:19:12 -0500

    quick additions

--- scripts/script.js
+++ scripts/script.js
@@ -14,7 +14,7 @@ $(document).ready(function(){
     var clientSide = Math.floor(Math.random()*(60 - 10 + 1) + 10);
     var serverSide = Math.floor(Math.random()*(60 - 10 + 1) + 10);
     // var requiredPoints = [frontEnd, clientSide, serverSide];
-    $('#projectIn').append("<div><h2>" + projectName + "</h2><p>Front End: " + frontEnd + "</p><p>Client Side: " + clientSide + "</p><p>Server Side: " + serverSide + "</p><button id='deleteProject'>Delete Project</button></div>")
+    $('#projectIn').append("<div><h2>" + projectName + "</h2><p>Front End: " + frontEnd + "</p><p>Client Side: " + clientSide + "</p><p>Server Side: " + serverSide + "</p><button id='deleteProject' class='btn btn-danger'>Delete Project</button><div id='employeeIn'><h2>Employees</h2></div></div>")
     openProject = true;
     $('#deleteProject').click(function(){
       $(this).parent().remove();
